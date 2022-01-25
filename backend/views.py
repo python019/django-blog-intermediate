9bb007d941cdf7121540b7cba23184cf47259120
python019 <82611064+python019@users.noreply.github.com>
2022-01-25 08:15:15 +0500

    4-dars

--- backend/views.py
+++ backend/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'blog/index.html')
+    return render(request, 'index.html')
--- blog/settings.py
+++ blog/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'frontend' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

a4a0b76c8dcad74a6d3e955d3a19b65bb5f397e3
python019 <82611064+python019@users.noreply.github.com>
2022-01-31 03:41:40 +0500

    9-dars

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    nomi = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nomi

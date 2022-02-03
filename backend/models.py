f649b168dd2185a1014718adcd116f986ad30ad6
python019 <82611064+python019@users.noreply.github.com>
2022-02-03 09:09:28 +0500

    base

--- backend/models.py
+++ backend/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     nomi = models.CharField(max_length=20)
+    rasm = models.ImageField(upload_to="rasmlar/")
 
     def __str__(self):
         return self.nomi
--- blog/settings.py
+++ blog/settings.py
@@ -124,6 +124,11 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

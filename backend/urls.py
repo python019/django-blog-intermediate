ec8f514bbd29de819fb8c31ddfb1662b915dcc68
python019 <82611064+python019@users.noreply.github.com>
2022-02-09 14:48:04 +0500

    16-dars

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home),
-    path('about/', about),
+    path('', home, name='home'),
+    path('about/', about, name='about'),
 ]

0e20501a0bdaf370f6cd3f0d9f0c907076ca0336
python019 <82611064+python019@users.noreply.github.com>
2022-02-09 13:55:04 +0500

    15-dars

--- backend/models.py
+++ backend/models.py
@@ -8,8 +8,10 @@ class Category(models.Model):
 
 
 class Post(models.Model):
+    tanlov = models.ForeignKey(Category, on_delete=models.CASCADE)
     nomi = models.CharField(max_length=20)
     rasm = models.ImageField(upload_to="rasmlar/")
+
 
     def __str__(self):
         return self.nomi

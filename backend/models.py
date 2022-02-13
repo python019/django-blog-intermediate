4d13e5bf8a03574450130c328aa8d74501e2db96
python019 <82611064+python019@users.noreply.github.com>
2022-02-14 03:30:10 +0500

    17-dars

--- backend/models.py
+++ backend/models.py
@@ -1,17 +1,26 @@
 from django.db import models
 
 class Category(models.Model):
-    nomi = models.CharField(max_length=20)
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+    name = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
-        return self.nomi
+        return self.name
 
 
 class Post(models.Model):
-    tanlov = models.ForeignKey(Category, on_delete=models.CASCADE)
-    nomi = models.CharField(max_length=20)
-    rasm = models.ImageField(upload_to="rasmlar/")
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=20, null=True, blank=True)
+    image = models.ImageField(upload_to="rasmlar/", null=True, blank=True)
 
 
     def __str__(self):
-        return self.nomi
+        return self.name

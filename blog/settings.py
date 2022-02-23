5a2d095b70c5c26083dd388e6cc8f22488b1a9fe
python019 <82611064+python019@users.noreply.github.com>
2022-02-23 10:54:07 +0500

    base

--- blog/settings.py
+++ blog/settings.py
@@ -124,11 +124,6 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / 'media'
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
